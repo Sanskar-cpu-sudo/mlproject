05967aca549593d77809607004807a8218ee8650
Sanskar-cpu-sudo <sanskarpetkar76@gmail.com>
2026-03-26 14:48:32 +0530

    Data injestion

--- src/exception.py
+++ src/exception.py
@@ -1,12 +1,10 @@
 import sys
-import logging
-
-## error detail present in sys . return type of sys gives 3 info first 2 are useless 3rd is good
-## it tells at which line and file error occurred
+from src.logger import logging
+## error detail present in sys . return type of sys gives 3 info first 2 are useless 3rd is good ## it tells at which line and file error occurred
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     
-    file_name = exc_tb.tb_frame.f_code.co_filename  # to get filename
+    file_name = exc_tb.tb_frame.f_code.co_filename
     
     error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name, exc_tb.tb_lineno, str(error)
@@ -16,10 +14,8 @@ def error_message_detail(error, error_detail: sys):
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail: sys):
-        super().__init__(error_message)  
+        super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
-    
-
